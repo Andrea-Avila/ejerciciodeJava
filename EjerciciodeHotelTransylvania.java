05bc871c2c6872a5c66bab8b79e22c78e169de28
Andrea Avila <andrea009795@gmail.com>
2023-06-16 09:54:45 -0600

    Se sube el primer ejercicio de la administracion de un hotel

---
+++ EjerciciodeHotelTransylvania.java
@@ -0,0 +1,21 @@
+package hotelTransylvania;
+
+public class EjerciciodeHotelTransylvania {
+	public static void main(String[] args) {
+		
+		boolean statusHabitacion =false;
+		byte ocupanteXPiso =5;
+		short cantidadHabitaciones =50;
+		int numeroPisos=10;
+		long clientesAnuales =100600;
+		float precioXHabitacion =1500.50f;
+		double gananciaAnual=5500000.7d;
+		System.out.println(statusHabitacion);
+		System.out.println(ocupanteXPiso);
+		System.out.println(cantidadHabitaciones);
+		System.out.println(numeroPisos);
+		System.out.println(clientesAnuales);
+		System.out.println(precioXHabitacion);
+		System.out.println(precioXHabitacion);
+	}
+}
